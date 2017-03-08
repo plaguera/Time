525351bc204e595e806b1419a90fe7e43bfbd005
Pedro Lagüera <laguerapedro@gmail.com>
2017-03-08 15:48:30 +0000

    Fixed Indentation

--- Time.java
+++ Time.java
@@ -1,12 +1,12 @@
 package time;
 
 /**
- * Programación de Aplicaciones Interactivas: Práctica 5 - Game Of Life
- * @author Pedro Miguel Lagüera Cabrera
- * Email: laguerapedro@gmail.com
- * Mar 7, 2017
- * Time.java
- */
+* Programación de Aplicaciones Interactivas: Práctica 5 - Game Of Life
+* @author Pedro Miguel Lagüera Cabrera
+* Email: laguerapedro@gmail.com
+* Mar 7, 2017
+* Time.java
+*/
 public class Time {
 
 	private final static int MAX_HOUR = 24;
@@ -14,129 +14,129 @@ public class Time {
 	private final static int MAX_SEC = 60;
 
 	private int hour   = 0;
-    private int minute = 0;
-    private int second = 0;
+	private int minute = 0;
+	private int second = 0;
 
-    public Time(int second, int minute, int hour) {
-        setTime(second, minute, hour);
-    }
+	public Time(int second, int minute, int hour) {
+		setTime(second, minute, hour);
+	}
 
-    public void setTime(int second, int minute, int hour) {
-        setHour(hour);
-        setMinute(minute);
-        setSecond(second);
-    }
+	public void setTime(int second, int minute, int hour) {
+		setHour(hour);
+		setMinute(minute);
+		setSecond(second);
+	}
 
-    /** @return the hour */
-    public int getHour() {
-        return hour;
-    }
+	/** @return the hour */
+	public int getHour() {
+		return hour;
+	}
 
-    /** @return the minute */
-    public int getMinute() {
-        return minute;
-    }
+	/** @return the minute */
+	public int getMinute() {
+		return minute;
+	}
 
-    /** @return the second */
-    public int getSecond() {
-        return second;
-    }
+	/** @return the second */
+	public int getSecond() {
+		return second;
+	}
 
-    /** @param new hour value [0-23] */
-    public void setHour(int hour) {
-        if (hour < 0 || hour >= MAX_HOUR)
-            throw new IllegalArgumentException("Not valid hour value");
-        this.hour = hour;
-    }
+	/** @param new hour value [0-23] */
+	public void setHour(int hour) {
+		if (hour < 0 || hour >= MAX_HOUR)
+			throw new IllegalArgumentException("Not valid hour value");
+		this.hour = hour;
+	}
 
-    /** @param new minute value [0-59] */
-    public void setMinute(int minute) {
-        if (minute < 0 || minute >= MAX_MIN)
-            throw new IllegalArgumentException("Not valid minute value");
-        this.minute = minute;
-    }
+	/** @param new minute value [0-59] */
+	public void setMinute(int minute) {
+		if (minute < 0 || minute >= MAX_MIN)
+			throw new IllegalArgumentException("Not valid minute value");
+		this.minute = minute;
+	}
 
-    /** @param new second value [0-59] */
-    public void setSecond(int second) {
-        if (second < 0 || second >= MAX_SEC)
-            throw new IllegalArgumentException("Not valid second value");
-        this.second = second;
-    }
+	/** @param new second value [0-59] */
+	public void setSecond(int second) {
+		if (second < 0 || second >= MAX_SEC)
+			throw new IllegalArgumentException("Not valid second value");
+		this.second = second;
+	}
 
-    /** @return instance of class of next second */
-    public Time nextSecond() {
-        try {
-            setTime(++second, minute, hour);
-            return this;
-        }
-        catch(IllegalArgumentException e) {}
+	/** @return instance of class of next second */
+	public Time nextSecond() {
+		try {
+			setTime(++second, minute, hour);
+			return this;
+		}
+		catch(IllegalArgumentException e) {
+			second = 0;
+			return nextMinute();
+		}
+	}
 
-        second = 0;
-        return nextMinute();
-    }
+	/** @return instance of class of next minute */
+	public Time nextMinute() {
+		try {
+			setTime(second, ++minute, hour);
+			return this;
+		}
+		catch(IllegalArgumentException e) {
+			minute = 0;
+			return nextHour();
+		}
+	}
 
-    /** @return instance of class of next minute */
-    public Time nextMinute() {
-        try {
-            setTime(second, ++minute, hour);
-            return this;
-        }
-        catch(IllegalArgumentException e) {}
+	/** @return instance of class of next hour */
+	public Time nextHour() {
+		try {
+			setTime(second, minute, ++hour);
+			return this;
+		}
+		catch(IllegalArgumentException e) {
+			hour = 0;
+			return this;
+		}
+	}
 
-        minute = 0;
-        return nextHour();
-    }
+	/** @return instance of class of previous second */
+	public Time previousSecond() {
+		try {
+			setTime(--second, minute, hour);
+			return this;
+		}
+		catch(IllegalArgumentException e) {
+			second = MAX_SEC-1;
+			return previousMinute();
+		}
+	}
 
-    /** @return instance of class of next hour */
-    public Time nextHour() {
-        try {
-            setTime(second, minute, ++hour);
-            return this;
-        }
-        catch(IllegalArgumentException e) {}
+	/** @return instance of class of previous minute */
+	public Time previousMinute() {
+		try {
+			setTime(second, --minute, hour);
+			return this;
+		}
+		catch(IllegalArgumentException e) {
+			minute = MAX_MIN-1;
+			return previousHour();
+		}
+	}
 
-        hour = 0;
-        return this;
-    }
+	/** @return instance of class of previous hour */
+	public Time previousHour() {
+		try {
+			setTime(second, minute, --hour);
+			return this;
+		}
+		catch(IllegalArgumentException e) {
+			hour = MAX_HOUR-1;
+			return this;
+		}
+	}
 
-    /** @return instance of class of previous second */
-    public Time previousSecond() {
-        try {
-            setTime(--second, minute, hour);
-            return this;
-        }
-        catch(IllegalArgumentException e) {}
-
-        second = MAX_SEC-1;
-        return previousMinute();
-    }
-
-    /** @return instance of class of previous minute */
-    public Time previousMinute() {
-        try {
-            setTime(second, --minute, hour);
-            return this;
-        }
-        catch(IllegalArgumentException e) {}
-
-        minute = MAX_MIN-1;
-        return previousHour();
-    }
-
-    /** @return instance of class of previous hour */
-    public Time previousHour() {
-        try {
-            setTime(second, minute, --hour);
-            return this;
-        }
-        catch(IllegalArgumentException e) {}
-
-        hour = MAX_HOUR-1;
-        return this;
-    }
-
-    public String toString() {
-    	return hour + ":" + minute + ":" + second;
-    }
+	public String toString() {
+		return hour + ":" + minute + ":" + second;
+	}
 
 }
--- TimeMain.java
+++ TimeMain.java
@@ -1,17 +1,17 @@
 package time;
 
 public class TimeMain {
-	
+
 	private final static int N_ITER = 1000;
 
 	public static void main(String[] args) {
-		
+
 		Time time = new Time(0, 0, 0);
 		for(int i = 0; i < 10; i++)
 			System.out.println(addSeconds(time));
 
 	}
-	
+
 	public static Time addSeconds(Time time) {
 		for(int i = 0; i < N_ITER; i++)
 			time.nextSecond();
